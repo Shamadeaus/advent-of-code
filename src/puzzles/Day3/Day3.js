fe9aded898d756f2f4778ac3883fb8fc3c2f0c1e
Shaun Curtis <scurtis@peoplenetonline.com>
2016-07-21 20:57:39 -0500

    update day 3

--- src/puzzles/Day3/Day3.js
+++ src/puzzles/Day3/Day3.js
@@ -1,146 +1,43 @@
-const fs = require('fs')
 const _ = require('lodash')
 
 module.exports = {
-    part1: () => {
-        let file = fs.readFileSync('src/puzzles/Day3/input.txt').toString()
-        let houses = []
-
-        let location = {
-            x: 0,
-            y: 0
+    run: (file) => {
+        let mover
+        let santaPart1 = {
+            lat: 0,
+            lon: 0,
+            houses: {}
         }
 
-        file.split('').reduce((preValue, curValue) => {
-            if(curValue === '^') {
-                let north = _.cloneDeep(location)
+        let santaPart2 = _.cloneDeep(santaPart1)
+        let roboSanta = _.cloneDeep(santaPart1)
 
-                north.y++
+        _.set(santaPart1.houses, `${santaPart1.lat}, ${santaPart1.lon}`, 1)
+        _.set(santaPart2.houses, `${santaPart2.lat}, ${santaPart2.lon}`, 1)
+        _.set(roboSanta.houses, `${roboSanta.lat}, ${roboSanta.lon}`, 1)
 
-                if (!_.some(houses, north)) {
-                    houses.push(north)
-                }
+        _.forEach(file.split(''), (curValue, index) => {
+            index % 2 === 1 ? mover = roboSanta : mover = santaPart2
 
-                location = north
+            if (curValue === '^') {
+                santaPart1.lat++
+                mover.lat++
             } else if (curValue === 'v') {
-                let south = _.cloneDeep(location)
-
-                south.y--
-
-                if (!_.some(houses, south)) {
-                    houses.push(south)
-                }
-
-                location = south
+                santaPart1.lat--
+                mover.lat--
             } else if (curValue === '>') {
-                let east = _.cloneDeep(location)
-
-                east.x++
-
-                if (!_.some(houses, east)) {
-                    houses.push(east)
-                }
-
-                location = east
+                santaPart1.lon++
+                mover.lon++
             } else if (curValue === '<') {
-                let west = _.cloneDeep(location)
-
-                west.x--
-
-                if (!_.some(houses, west)) {
-                    houses.push(west)
-                }
-
-                location = west
+                santaPart1.lon--
+                mover.lon--
             }
 
+            _.set(santaPart1.houses, `${santaPart1.lat}, ${santaPart1.lon}`, 1)
+            _.set(mover.houses, `${mover.lat}, ${mover.lon}`, 1)
         })
 
-        return houses.length+1
+        return `Part 1: ${_.size(santaPart1.houses)}, Part 2: ${_.size(_.merge(santaPart2.houses, roboSanta.houses))}`
 
-    },
-    part2: () => {
-        let file = fs.readFileSync('src/puzzles/Day3/input.txt').toString()
-        let houses = []
-
-        let santaLocation = {
-            x: 0,
-            y: 0
-        }
-
-        let roboSantaLocation = {
-            x: 0,
-            y: 0
-        }
-
-        file.split('').reduce((preValue, curValue) => {
-            if(curValue === '^') {
-                let snorth = _.cloneDeep(santaLocation)
-                let rsouth = _.cloneDeep(roboSantaLocation)
-
-                snorth.y++
-                rsouth.y--
-
-                if (!_.some(houses, snorth)) {
-                    houses.push(snorth)
-                }
-                if (!_.some(houses, rsouth)) {
-                    houses.push(rsouth)
-                }
-
-                santaLocation = snorth
-                roboSantaLocation = rsouth
-            } else if (curValue === 'v') {
-                let ssouth = _.cloneDeep(santaLocation)
-                let rnorth = _.cloneDeep(roboSantaLocation)
-
-                ssouth.y--
-                rnorth.y++
-
-                if (!_.some(houses, ssouth)) {
-                    houses.push(ssouth)
-                }
-                if (!_.some(houses, rnorth)) {
-                    houses.push(rnorth)
-                }
-
-                santaLocation = ssouth
-                roboSantaLocation = rnorth
-            } else if (curValue === '>') {
-                let seast = _.cloneDeep(santaLocation)
-                let rwest = _.cloneDeep(roboSantaLocation)
-
-                seast.x++
-                rwest.x--
-
-                if (!_.some(houses, seast)) {
-                    houses.push(seast)
-                }
-                if (!_.some(houses, rwest)) {
-                    houses.push(rwest)
-                }
-
-                santaLocation = seast
-                roboSantaLocation = rwest
-            } else if (curValue === '<') {
-                let swest = _.cloneDeep(santaLocation)
-                let reast = _.cloneDeep(roboSantaLocation)
-
-                swest.x--
-                reast.x++
-
-                if (!_.some(houses, swest)) {
-                    houses.push(swest)
-                }
-                if (!_.some(houses, reast)) {
-                    houses.push(reast)
-                }
-
-                santaLocation = swest
-                roboSantaLocation = reast
-            }
-        })
-
-        return houses.length+2
     }
 }
